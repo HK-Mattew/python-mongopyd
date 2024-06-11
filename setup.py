6f84ebb0226f912234618b381e55b96d43c0ac6a
HK-Mattew <mattewsub6565@gmail.com>
2024-06-11 11:01:57 -0300

    Fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
       author_email='not-found@localhost.local',
       url='https://github.com/HK-Mattew/python-mongopyd',
       packages=find_packages(),
-      requires=get_requirements(),
+      install_requires=get_requirements(),
       license='MIT license',
       keywords=['mongodb orm', 'mongodb model', 'pymongo', 'motor', 'pydantic']
       )
